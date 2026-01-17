c2fc7512a6f2e1b7b0df65283f72e932ad184bda
Harsh Verma <harshverma3105006@gmail.com>
2026-01-18 02:44:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Portfolio Loaded Successfully");
+const reveals = document.querySelectorAll('.reveal');
+
+window.addEventListener('scroll', () => {
+  for (let reveal of reveals) {
+    const windowHeight = window.innerHeight;
+    const revealTop = reveal.getBoundingClientRect().top;
+    const revealPoint = 150;
+
+    if (revealTop < windowHeight - revealPoint) {
+      reveal.classList.add('active');
+    }
+  }
+});
+
